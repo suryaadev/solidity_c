6a843b6bf7c0e7a4e2002d9b8f1f428b980e6c88
suryaadev <infiniterohitsurya@gmail.com>
2022-07-26 12:30:51 +0530

    variable storage keyword

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -21,7 +21,16 @@ contract SimpleStorage{
     function restore() public view returns(uint256){
         return favNumber;
     }
+/**
+    lets see what are memory, storage or calldata there are different places where we can store the variables but this are common 
+    1. calldata - it works like temporery we can't modify it over the course 
+    2. memory - they can be modified
+    3. storage - there scope is out on function also they also can be modified
 
+    this all applicable only on arrays, mapping etc. 
+    string is itself array behind the hood so here we use memory
+
+ */
     function addPerson(uint256 _number, string memory _name) public {
         
        /* Person memory newPerson = Person({number : _number, name : _name});
